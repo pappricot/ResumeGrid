798358d38073332d47bec82abd2517503cb2cf26
Anya <anya.p.nguyen@gmail.com>
2020-04-14 19:52:03 -0700

    added email function

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
 function goBack() {
   window.history.back()
 }
+const form = document.getElementsByTagName('form')
+form.addEventListener('submit', contact, false)
+function contact(e) {
+  // Prevent Default Form Submission
+  e.preventDefault()
+
+  let target = e.target || e.srcElement
+  let i = 0
+  let message = ''
+
+  // Loop Through All Input Fields
+  for (i = 0; i < target.length; ++i) {
+    // Check to make sure it's a value. Don't need to include Buttons
+    if (target[i].type != 'text' && target[i].type != 'textarea') {
+      // Skip to next input since this one doesn't match our rules
+      continue
+    }
+
+    // Add Input Name and value followed by a line break
+    message += target[i].name + ': ' + target[i].value + '\r\n'
+  }
+  // Modify the hidden body input field that is required for the mailto: scheme
+  target.elements['body'].value = message
+
+  // Submit the form since we previously stopped it. May cause recursive loop in some browsers? Should research this.
+  this.submit()
+}
